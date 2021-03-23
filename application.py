f4c33368c08fa70f73b03635fda9bb7acf76b1b0
Thomas Newton <tomnewtonx@gmail.com>
2021-03-23 11:03:00 +0000

    os.env

--- application.py
+++ application.py
@@ -44,8 +44,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-#db = SQL(os.getenv("DATABASE_URL"))
-db = SQL(os.environ.get('DATABASE_URL'))
+db = SQL(os.getenv("DATABASE_URL"))
 
 # Create transaction table. 
                 
