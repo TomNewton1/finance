daad5ec4aac25278363f4b84c0ab16dc42618cd9
Thomas Newton <tomnewtonx@gmail.com>
2021-03-27 10:26:18 +0000

    dbcommand

--- commands.py
+++ commands.py
@@ -1,3 +1,9 @@
+import click
+from flask.cli import with_appcontext
+
+from .application import db
+from .application import transaction, users
+
 @click.command(name='create_tables')
 @with_appcontext
 def create_tables():
